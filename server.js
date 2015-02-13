3495bfffaf4be6faaad0306a57fb99960588dcf5
Christopher Curtis <ccurtisj@gmail.com>
2015-02-13 08:01:24 -0600

    Production db connection

--- server.js
+++ server.js
@@ -4,7 +4,26 @@ var express           = require('express'),
     mongoose          = require('mongoose'),
     genresController  = require('./server/controllers/genres-controller');
 
-mongoose.connect('mongodb://localhost:27017/subgenre');
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring =
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/subgenre';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 27017;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 app.listen(3000, function(){
   console.log("I'm listening...");
