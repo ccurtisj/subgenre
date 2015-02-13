8687937092ff91160758c3c03a6efad7ac022a5a
Christopher Curtis <ccurtisj@gmail.com>
2015-02-13 07:45:32 -0600

    Display nested list

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,6 @@
-var app = angular.module('subgenreApp', ['ngResource']);
+var underscore = angular.module('underscore', []);
+underscore.factory('_', function() {
+  return window._; //Underscore must already be loaded on the page
+});
+
+var app = angular.module('subgenreApp', ['ngResource','underscore']);
--- client/js/controllers/genres-controller.js
+++ client/js/controllers/genres-controller.js
@@ -1,9 +1,37 @@
-app.controller('genresController', ['$scope',  '$resource', function($scope, $resource){
+app.controller('genresController', ['$scope',  '$resource', '_', function($scope, $resource, _){
 
-  Genre = $resource('/api/genres')
+  Genre = $resource('/api/genres');
+
+  var updateGenreGroups = function(){
+    var groupedGenres = _.reduce($scope.genres, function(memo, genre){
+
+      var genreLabel = null
+      if(typeof(genre.parentGenre) == 'undefined'){
+        genreLabel = genre.name;
+      } else{
+        genreLabel = genre.parentGenre;
+      };
+
+      var genreHash = memo[genreLabel] || { label: genreLabel, subGenres: [] }
+
+      if(typeof(genre.parentGenre) == 'undefined'){
+        genreHash.parentGenre = genre
+      } else{
+        genreHash.subGenres.push(genre)
+      }
+
+      memo[genreLabel] = genreHash;
+      return memo;
+    }, {});
+
+    // console.log(genresHash)
+    $scope.groupedGenres = groupedGenres;
+  }
 
   Genre.query(function(results){
     $scope.genres = results;
+    updateGenreGroups();
+    // console.log($scope.genres);
   })
 
   $scope.createGenre = function(){
@@ -14,6 +42,7 @@ app.controller('genresController', ['$scope',  '$resource', function($scope, $re
     genre.$save(function(result){
       $scope.genres.push(result);
       $scope.genreName  = '';
+      updateGenreGroups();
     });
   }
 }]);
